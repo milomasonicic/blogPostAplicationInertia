35c5a3f1d8cf7eb8c9b6f63a8c639088434edf03
milo.masonicic <ilom3774@gmail.com>
2023-10-07 19:08:18 +0200

    first commit

---
+++ resources/js/Pages/blogPosts.vue
@@ -0,0 +1,30 @@
+<script>
+
+import { Link } from '@inertiajs/vue3'
+
+export default{
+    props:{
+        posts:{
+            type:Object
+        }
+    }, 
+    components:{
+        Link
+    }
+}
+
+</script>
+
+<template>
+    <h1>Posts</h1>
+
+    <div v-for="post in posts" :key="post.id">
+        
+        <h1> {{ post.title }}</h1>
+        <p> {{ post.content }}</p>
+        
+        <Link :href="route('singlePost', { id: post.id })"> View Post</Link>
+
+    </div>
+
+</template>
---
+++ resources/js/Pages/singlePost.vue
@@ -0,0 +1,23 @@
+<script>
+
+
+export default{
+    props:{
+        post:{
+            type:Object
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Posts</h1>
+
+
+    
+    <h1> {{ post.title }}</h1>
+    <p> {{ post.content }}</p>
+    
+ 
+
+</template>
